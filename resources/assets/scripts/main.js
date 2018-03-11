25d48996a53cd6e616ef97b82845bf63292221fb
Jason Baciulis <jason.baciulis@gmail.com>
2018-03-11 18:02:18 +0700

    chore: Add fontawesome 5 icons

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -10,6 +10,15 @@ import common from './routes/common';
 import home from './routes/home';
 import aboutUs from './routes/about';
 
+// base package
+import fontawesome from "@fortawesome/fontawesome";
+// Facebook and Twitter icons
+import faFacebook from "@fortawesome/fontawesome-free-brands/faFacebook";
+import faTwitter from "@fortawesome/fontawesome-free-brands/faTwitter";
+
+// add the imported icons to the library
+fontawesome.library.add(faFacebook, faTwitter);
+
 /** Populate Router instance with DOM routes */
 const routes = new Router({
   // All pages
