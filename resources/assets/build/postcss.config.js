f4b0971acadd1fe962ab53f915c72918a90ae722
Jason Baciulis <jason.baciulis@gmail.com>
2018-03-11 14:17:53 +0700

    chore: unCSS config

--- resources/assets/build/postcss.config.js
+++ resources/assets/build/postcss.config.js
@@ -4,10 +4,18 @@ const cssnanoConfig = {
   preset: ['default', { discardComments: { removeAll: true } }]
 };
 
+const uncssConfig = {
+  html: [
+    'http://example.test',
+    // Your entire sitemap added manually
+  ]
+};
+
 module.exports = ({ file, options }) => {
   return {
     parser: options.enabled.optimize ? 'postcss-safe-parser' : undefined,
     plugins: {
+      'postcss-uncss': options.enabled.optimize ? uncssConfig : false,
       cssnano: options.enabled.optimize ? cssnanoConfig : false,
       autoprefixer: true,
     },
