661e147a546d38c3cd4bcfe610547745d3ea808b
Jason Baciulis <jason.baciulis@gmail.com>
2018-04-17 21:41:04 +0700

    Set up ACF to pull as variables

--- app/controllers/front-page.php
+++ app/controllers/front-page.php
@@ -6,5 +6,20 @@ use Sober\Controller\Controller;
 
 class FrontPage extends Controller
 {
+    // Set up to pull in ACF plugin fields as variables
+    public function product_intro_heading() {
+        return get_field('product_intro_heading');
+    }
 
+    public function product_intro_description() {
+        return get_field('product_intro_description');
+    }
+
+    public function product_intro_image() {
+        return get_field('product_intro_image');
+    }
+
+    public function product_price() {
+        return get_field('product_price');
+    }
 }
