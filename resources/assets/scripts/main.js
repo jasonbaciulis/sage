db75d50050885f30f2c28a8e0ea59f97357315d2
Jason Baciulis <jason.baciulis@gmail.com>
2018-04-25 16:04:59 +0700

    Add comments about font awesome

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -1,22 +1,23 @@
-// import external dependencies
+// Import external dependencies
 import 'jquery';
 
 // Import everything from autoload
 import "./autoload/**/*"
 
-// import local dependencies
+// Import local dependencies
 import Router from './util/Router';
 import common from './routes/common';
 import home from './routes/home';
 import aboutUs from './routes/about';
 
-// base package
+// Base Font Awesome 5 package
+// How to use it: https://discourse.roots.io/t/how-to-use-font-awesome-5-in-your-sage-theme/11737
 import fontawesome from "@fortawesome/fontawesome";
 // Facebook and Twitter icons
 import faFacebook from "@fortawesome/fontawesome-free-brands/faFacebook";
 import faTwitter from "@fortawesome/fontawesome-free-brands/faTwitter";
 
-// add the imported icons to the library
+// Add the imported icons to the library
 fontawesome.library.add(faFacebook, faTwitter);
 
 /** Populate Router instance with DOM routes */
