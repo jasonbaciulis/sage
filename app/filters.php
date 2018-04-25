8f128eb1da31870558313ee52199f2dd7b8c050d
Jason Baciulis <jason.baciulis@gmail.com>
2018-04-25 15:49:34 +0700

    Update async defer filters to use handles

--- app/filters.php
+++ app/filters.php
@@ -77,6 +77,7 @@ add_filter('comments_template', function ($comments_template) {
 
 /**
  * Add preconnect for Google Fonts.
+ * More info: https://www.igvita.com/2015/08/17/eliminating-roundtrips-with-preconnect/
  *
  * @since Twenty Seventeen 1.0
  *
@@ -97,102 +98,57 @@ add_filter( 'wp_resource_hints', function( $urls, $relation_type ) {
 
 
 /**
- * Add custom image sizes attribute to enhance responsive image functionality
- * for content images.
- *
- * @origin Twenty Seventeen 1.0
- *
- * @param string $sizes A source size value for use in a 'sizes' attribute.
- * @param array  $size  Image size. Accepts an array of width and height
- *                      values in pixels (in that order).
- * @return string A source size value for use in a content image 'sizes' attribute.
- */
-add_filter( 'wp_calculate_image_sizes', function ( $sizes, $size ) {
-	if ( is_singular() ) {
-		$width = $size[0];
-		if ( 610 <= $width ) {
-			$sizes = '(min-width: 990px) 720px, (min-width: 1300px) 610px, 95vw';
-		}
-		return $sizes;
-	}
-}, 10, 2 );
-
-
-/**
- * Add custom image sizes attribute to enhance responsive image functionality
- * for post thumbnails.
- *
- * @origin Twenty Seventeen 1.0
- *
- * @param array $attr       Attributes for the image markup.
- * @param int   $attachment Image attachment ID.
- * @param array $size       Registered image size or flat array of height and width dimensions.
- * @return string A source size value for use in a post thumbnail 'sizes' attribute.
- */
-add_filter( 'wp_get_attachment_image_attributes', function ( $attr, $attachment, $size ) {
-	if ( is_singular() ) {
-		$attr['sizes'] = '(min-width: 990px) 720px, (min-width: 1300px) 820px, 95vw';
-	} else {
-		$attr['sizes'] = '(min-width: 990px) 955px, (min-width: 1300px) 966px, 95vw';
-	}
-	return $attr;
-}, 10, 3 );
-
-
-/**
  * Defer all scripts which are not excluded
  */
-add_filter( 'script_loader_tag', function($tag) {
-	if (is_admin()) {
-        return $tag;
-    }
-	// Do not add defer or async attribute to these scripts
-	$scripts_to_exclude = []; // add a string of js file e.g. script.js
+// add_filter( 'script_loader_tag', function($tag, $handle) {
+// 	if (is_admin()) {
+//         return $tag;
+//     }
+// 	// Add a script handle to NOT defer it
+// 	$scripts_to_exclude = [];
 
-	foreach($scripts_to_exclude as $exclude_script) {
-		if (true == strpos($tag, $exclude_script ) )
-			return $tag;
-	}
-	// Defer all remaining scripts not excluded above
-	return str_replace( ' src', ' defer src', $tag );
-}, 10, 1);
+// 	foreach($scripts_to_exclude as $exclude_script) {
+// 		if ($handle === $exclude_script)
+// 			return $tag;
+// 	}
+// 	// Defer all remaining scripts not excluded above
+// 	return str_replace( ' src', ' defer src', $tag );
+// });
 
 
 /**
  *  Defer only scripts which are included in here
  */
-// add_filter('script_loader_tag', function($tag) {
+// add_filter('script_loader_tag', function($tag, $handle) {
 // 	if (is_admin()) {
 // 		return $tag;
 // 	}
-// 	// add a string of js file to defer e.g. script.js
+// 	//  Add a script handle to defer it
 // 	$scripts_to_include = [];
 
 // 	foreach($scripts_to_include as $include_script) {
-// 		// finds the position of the first occurence of include_script inside tag
-// 		if (false == strpos($tag, $include_script ) )
-// 			return($tag);
+// 		if ($handle !== $include_script)
+// 			return $tag;
 // 	}
 // 	// Defer all scripts included above
 // 	return str_replace(' src', ' defer src', $tag);
-// }, 10, 1);
+// });
 
 
 /**
  *  Async only scripts which are included in here
  */
-add_filter('script_loader_tag', function($tag) {
-	if (is_admin()) {
-		return $tag;
-	}
-	// add a string of js file to async e.g. script.js
-	$scripts_to_include = [];
+// add_filter('script_loader_tag', function($tag, $handle) {
+// 	if (is_admin()) {
+// 		return $tag;
+// 	}
+// 	// Add a script handle to async
+// 	$scripts_to_include = [];
 
-	foreach($scripts_to_include as $include_script) {
-		// finds the position of the first occurence of include_script inside tag
-		if (false == strpos($tag, $include_script ) )
-			return($tag);
-	}
-	// async all scripts included above
-	return str_replace(' src', ' async src', $tag);
-}, 10, 1);
+// 	foreach($scripts_to_include as $include_script) {
+// 		if ($handle !== $include_script )
+// 			return $tag;
+// 	}
+// 	// Async all scripts included above
+// 	return str_replace(' src', ' async src', $tag);
+// });
