bb364f2d6ff1258caceb3a72b5541694d33c6cb3
Jason Baciulis <jason.baciulis@gmail.com>
2018-04-25 16:09:18 +0700

    Import ResponsiveBackgroundImage class and init

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,12 @@
+import ResponsiveBackgroundImage from '..libs/respbgimages.js';
+
 export default {
   init() {
     // JavaScript to be fired on all pages
+    const bgImages = document.querySelectorAll('.js-responsive-bg-image');
+    for (let i = 0; i < bgImages.length; i++) {
+      new ResponsiveBackgroundImage(bgImages[i]);
+    }
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
