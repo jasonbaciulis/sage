b87a5833fa62e997ce351a31bfdffd23a0b2cfee
Jason Baciulis <jason.baciulis@gmail.com>
2018-04-25 16:06:31 +0700

    Add script for responsive background images

---
+++ resources/assets/scripts/libs/respbgimages.js
@@ -0,0 +1,33 @@
+/**
+ * Code borrowed from Alexander Claes and slightly reworked
+ * https://aclaes.com/responsive-background-images-with-srcset-and-sizes/
+ */
+
+// Searches for inner img and grabs src to apply as background-image
+export class ResponsiveBackgroundImage {
+
+  constructor(element) {
+    this.element = element;
+    this.img = element.querySelector('img');
+    this.src = '';
+
+    this.img.addEventListener('load', () => {
+      this.update();
+    });
+
+    if (this.img.complete) {
+      this.update();
+    }
+  }
+
+  update() {
+    let src = typeof this.img.currentSrc !== 'undefined' ? this.img.currentSrc : this.img.src;
+    if (this.src !== src) {
+      this.src = src;
+      this.element.style.backgroundImage = 'url("' + this.src + '")';
+      // Add class to remove blur filter or make other reveal animation
+      this.element.classList.add('u-bg-image-loaded');
+    }
+  }
+}
+
