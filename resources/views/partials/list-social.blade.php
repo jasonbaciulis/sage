ef141f9971b449f3ff57d1cfd65f063cf1b1d5dd
Jason Baciulis <jason.baciulis@gmail.com>
2018-04-26 17:55:18 +0700

    Add blade partial for social media list

---
+++ resources/views/partials/list-social.blade.php
@@ -0,0 +1,15 @@
+<ul class="c-social-list">
+  <li class="c-social-list__item">
+    <a href="{{ $url_social_facebook }}" target="_blank" rel="noopener">
+      <i class="fas fa-facebook" aria-hidden="true"></i>
+      <span class="sr-only">Facebook</span>
+    </a>
+  </li>
+
+  <li class="c-social-list__item">
+    <a href="{{ $url_social_twitter }}" target="_blank" rel="noopener">
+      <i class="fas fa-twitter" aria-hidden="true"></i>
+      <span class="sr-only">Twitter</span>
+    </a>
+  </li>
+</ul>
