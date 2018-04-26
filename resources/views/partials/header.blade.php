c8e5eea82e8d0198b64693621c12bee611f1de92
Jason Baciulis <jason.baciulis@gmail.com>
2018-04-26 17:14:06 +0700

    Add bootstrap navbar markup

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,10 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
+    <nav class="navbar navbar-expand-md navbar-light">
+      <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu($primary_menu) !!}
       @endif
