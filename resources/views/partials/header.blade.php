59cab04cec8bfdbfeda4e2d3a5b5d4b4c791cb1c
Jason Baciulis <jason.baciulis@gmail.com>
2018-04-28 22:43:18 +0700

    Accessible logo link

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,10 @@
 <header class="banner">
   <div class="container">
     <nav class="navbar navbar-expand-md navbar-light">
-      <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+      <a class="brand" href="{{ home_url('/') }}" rel="home" aria-label="Home link">
+        <svg class="logo"></svg>
+        <span class="sr-only">{{ get_bloginfo('name', 'display') }}</span>
+      </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
