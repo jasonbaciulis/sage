f3bdcd3b39a2a2591462d3ddb6961673a995644d
Jason Baciulis <jason.baciulis@gmail.com>
2018-05-06 21:06:07 +0700

    Shorter class name

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -3,7 +3,7 @@ import ResponsiveBackgroundImage from '../libs/respbgimages.js';
 export default {
   init() {
     // JavaScript to be fired on all pages
-    const bgImages = document.querySelectorAll('.js-resp-bg-image');
+    const bgImages = document.querySelectorAll('.js-bg-image');
     for (let i = 0; i < bgImages.length; i++) {
       new ResponsiveBackgroundImage(bgImages[i]);
     }
