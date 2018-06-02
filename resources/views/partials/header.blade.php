437cff3bbfc57351fec538b2fc57e08f330f86d0
Jason Baciulis <jason.baciulis@gmail.com>
2018-06-02 19:18:19 +0700

    feat: Hamburger button

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,22 @@
 <header class="banner">
   <div class="container">
     <nav class="navbar navbar-expand-md navbar-light">
+
       <a class="brand" href="{{ home_url('/') }}" rel="home" aria-label="Home link">
         <svg class="logo"></svg>
         <span class="sr-only">{{ get_bloginfo('name', 'display') }}</span>
       </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+
+      <button class="c-hamburger  c-hamburger--slider  js-hamburger" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="c-hamburger__box">
+          <span class="c-hamburger__inner"></span>
+        </span>
       </button>
+
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu($primary_menu) !!}
       @endif
+
     </nav>
   </div>
 </header>
