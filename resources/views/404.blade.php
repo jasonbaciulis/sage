9b4c4ebac4470dfcc1dff6291738569ba462a109
Jason Baciulis <jason.baciulis@gmail.com>
2018-06-02 19:17:36 +0700

    feat: Default 404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,12 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+<section class="o-section  o-section--404">
+  <div class="container">
+    <div class="row  justify-content-center">
+      <div class="col-12  col-lg-7  u-text-center">
+        <h1>404</h1>
+        <p class="c-lead">{{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}</p>
+        <p class="c-lead">{{ __('Why don\'t you try the homepage?', 'sage') }}</p>
+        <a href="{{ home_url('/') }}" class="c-btn  c-btn--primary">{{ __('Go to homepage', 'sage')}}</a>
+      </div>
     </div>
-    {!! get_search_form(false) !!}
-  @endif
+  </div>
+</section>
+
 @endsection
