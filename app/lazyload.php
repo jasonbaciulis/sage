02cf6ed20b02bcb83f85e2b9679da435f517e45b
Jason Baciulis <jason@punkmypixel.com>
2018-10-01 22:53:22 +0300

    Format psr-2

--- app/lazyload.php
+++ app/lazyload.php
@@ -9,41 +9,40 @@
  * @link https://github.com/aFarkas/lazysizes
  */
 
-add_filter( 'the_content',  function ($content) {
-    if ( empty( $content ) ) {
-       return $content;
+add_filter('the_content', function ($content) {
+    if (empty($content)) {
+        return $content;
     }
 
     $dom = new DOMDocument();
-    libxml_use_internal_errors( true );
-    $dom->loadHTML( $content );
+    libxml_use_internal_errors(true);
+    $dom->loadHTML($content);
     libxml_clear_errors();
 
-    foreach ( $dom->getElementsByTagName( 'img' ) as $img ) {
-        if ( $img->hasAttribute( 'srcset' ) ) {
-
+    foreach ($dom->getElementsByTagName('img') as $img) {
+        if ($img->hasAttribute('srcset')) {
             $sizes = 'auto';
-            $classes = $img->getAttribute( 'class' );
-            $src = $img->getAttribute( 'src' );
-            $srcset = $img->getAttribute( 'srcset' );
-            $width = $img->getAttribute( 'width' );
-            $height = $img->getAttribute( 'height' );
+            $classes = $img->getAttribute('class');
+            $src = $img->getAttribute('src');
+            $srcset = $img->getAttribute('srcset');
+            $width = $img->getAttribute('width');
+            $height = $img->getAttribute('height');
             // Calculate aspect ratio for lazysizes plugin to size image correctly
             $aspectratio = "$width/$height";
             $classes .= ' lazyload';
 
-            $img->setAttribute( 'src', 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==' );
-            $img->setAttribute( 'data-src', $src );
-            $img->setAttribute( 'data-srcset', $srcset );
-            $img->setAttribute( 'data-sizes', $sizes );
-            $img->setAttribute( 'data-aspectratio', $aspectratio );
-            $img->setAttribute( 'class', $classes );
+            $img->setAttribute('src', 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==');
+            $img->setAttribute('data-src', $src);
+            $img->setAttribute('data-srcset', $srcset);
+            $img->setAttribute('data-sizes', $sizes);
+            $img->setAttribute('data-aspectratio', $aspectratio);
+            $img->setAttribute('class', $classes);
 
-            $img->removeAttribute( 'sizes' );
-            $img->removeAttribute( 'srcset' );
+            $img->removeAttribute('sizes');
+            $img->removeAttribute('srcset');
 
             $content = $dom->saveHTML();
-       }
+        }
     }
     return $content;
 }, 20, 1);
