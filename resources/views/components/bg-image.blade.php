7c59ec7650855d01996d3ebb2ed97a898a3c6717
Jason Baciulis <jason@punkmypixel.com>
2018-10-02 17:04:48 +0300

    Namespace

--- resources/views/components/bg-image.blade.php
+++ resources/views/components/bg-image.blade.php
@@ -1,5 +1,5 @@
-<section class="bg-image {{ $class ?? '' }}" style="background-image:linear-gradient()">
-  <img src="{{ App::get_img_src($image, 'fullhd') }}" style="display:none" sizes="100vw" srcset="{{ App::get_img_srcset($image) }}">
+<section class="c-bg-image {{ $class ?? '' }}" style="background-image:linear-gradient()">
+  <img src="{{ App::get_img_src($image, 'full') }}" style="display:none" sizes="100vw" srcset="{{ App::get_img_srcset($image) }}">
   <div class="container">
     {!! $slot !!}
   </div>
