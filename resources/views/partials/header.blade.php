e9cddd3d67e5138ff4a2f4de28ba5e2ff98c143f
Jason Baciulis <jason@punkmypixel.com>
2018-10-21 18:29:45 +0300

    Update header classes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,22 +1,24 @@
-<header class="banner">
+<header class="c-navbar">
   <div class="container">
-    <nav class="navbar navbar-expand-md navbar-light">
+    <div class="d-flex  align-items-center  justify-content-lg-between">
 
-      <a class="brand" href="{{ home_url('/') }}" rel="home" aria-label="Home link">
-        <svg class="logo"></svg>
-        <span class="sr-only">{{ get_bloginfo('name', 'display') }}</span>
+      <a href="{{home_url('/')}}" class="c-navbar__brand" rel="home" aria-label="Home link">
+        {{-- <svg class="logo"></svg> --}}
+        <span class="sr-only">{{get_bloginfo('name', 'display')}}</span>
       </a>
 
-      <button class="c-hamburger  c-hamburger--slider  js-hamburger" type="button" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="c-hamburger  c-hamburger--slider  js-hamburger" type="button" aria-controls="nav-primary" aria-expanded="false" aria-label="Toggle navigation">
         <span class="c-hamburger__box">
           <span class="c-hamburger__inner"></span>
         </span>
       </button>
 
-      @if (has_nav_menu('primary_navigation'))
-        {!! App\bem_nav_menu( 'primary_navigation', 'c-primary-nav', 'c-primary-nav__list' ) !!}
-      @endif
+      <nav id="nav-primary" class="c-nav-primary">
+        @if (has_nav_menu('primary_navigation'))
+          {!! App\bem_nav_menu( 'primary_navigation', 'c-primary-nav', 'c-primary-nav__list' ) !!}
+        @endif
+      </nav>
 
-    </nav>
+    </div>
   </div>
 </header>
