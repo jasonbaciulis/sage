e7a290f25412949d8b6296fa608c07d40519a854
Jason Baciulis <jason@punkmypixel.com>
2018-10-21 18:30:00 +0300

    Update image

--- resources/views/partials/image.blade.php
+++ resources/views/partials/image.blade.php
@@ -1,13 +1,12 @@
 @php
-$id    = $id ?? get_post_thumbnail_id();
-$size  = $size ?? 'medium';
-$ratio = $ratio ?? '1by1';
-$img   = !empty($id) ? App::get_img_object($id, $size) : null;
+$id   = $id ?? get_post_thumbnail_id();
+$size = $size ?? 'full';
+$img  = App::get_img_object($id, $size) ?? null;
 @endphp
 
-@if ($img)
-<figure class="c-ratio">
-  <div class="c-ratio__fill--{{$ratio}}"></div>
-  <img src="{{$img->placeholder}}" class="c-ratio__content  lazyload" data-srcset="{{$img->srcset}}" data-src="{{$img->src}}" data-sizes="auto" alt="{{$img->alt}}">
-</figure>
-@endif
+@isset($img)
+  <figure class="o-ratio">
+    <div style="padding-bottom:{{$img->ratio_percent}}"></div>
+    <img src="{{$img->placeholder}}" class="o-ratio__content  lazyload" data-srcset="{{$img->srcset}}" data-src="{{$img->src}}" data-sizes="auto" alt="{{$img->alt}}">
+  </figure>
+@endisset
