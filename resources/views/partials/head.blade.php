ddf048710c2307584981796c5a15b0afa851fa76
Jason Baciulis <jason@punkmypixel.com>
2018-10-21 18:29:31 +0300

    Remove comments

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,8 +2,5 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    {{-- Preload logo to show it ASAP
-    @see https://images.guide/#preload-critical-image-assets   --}}
-  <link rel="preload" as="image" href="@asset('images/logo.svg')" />
   @php wp_head() @endphp
 </head>
