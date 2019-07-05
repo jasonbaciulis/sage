f489cf3624beea3d8ddf34b0aff05ae849908aa8
Jason Baciulis <jason.baciulis@gmail.com>
2019-07-05 16:40:08 +0300

    fix: Close icon

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -1,3 +1,3 @@
-<svg class="c-icon {{ $class ?? null }}" fill="currentColor" aria-hidden="true" role="img" focusable="false">
-  <use href="#{{ $id }}" xlink:href="#{{ $id }}">
+<svg class="c-icon {{ $class ?? null }}" fill="currentColor" aria-hidden="true" focusable="false">
+  <use href="#{{ $id }}" xlink:href="#{{ $id }}"/>
 </svg>
